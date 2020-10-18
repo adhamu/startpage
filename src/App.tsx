0c7cfe9517459b03b88a2e90c3d5984d6fbdf8c7
Amit Dhamu <dev@amitd.co>
2020-10-18 16:14:13 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+
+const App = (): JSX.Element => <>Hello world!</>
+
+export default App
---
+++ webpack.config.ts
@@ -0,0 +1,49 @@
+import * as path from 'path'
+import * as HtmlWebPackPlugin from 'html-webpack-plugin'
+import * as webpack from 'webpack'
+
+const config: webpack.Configuration = {
+  entry: './src/index.tsx',
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      '@global': path.resolve(__dirname, 'src/'),
+    },
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/templates/index.html',
+      filename: './index.html',
+    }),
+  ],
+}
+
+export default config
