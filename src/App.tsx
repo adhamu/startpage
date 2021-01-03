fe22ceb9dc23b0513984f068d7b1f6269ceb60bb
Amit Dhamu <dev@amitd.co>
2021-01-03 17:25:06 +0000

    Basic commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,44 @@
 import * as React from 'react'
+import { Global, css } from '@emotion/react'
+import styled from '@emotion/styled'
 
-const App = (): JSX.Element => <>Hello world!</>
+const timeOfDay = () => {
+  const hour = new Date().getHours()
+
+  if (hour >= 5 && hour < 12) {
+    return 'morning'
+  } else if (hour >= 12 && hour < 17) {
+    return 'afternoon'
+  } else if ((hour >= 17 && hour <= 23) || hour < 5) {
+    return 'evening'
+  }
+}
+
+const GlobalStyle = css`
+  * {
+    padding: 0;
+    margin: 0;
+  }
+  body {
+    font-family: 'Open Sans', cursive;
+    font-size: 16px;
+    line-height: 28px;
+  }
+`
+
+const Greeting = styled.div`
+  display: grid;
+  place-content: center;
+  height: 100%;
+  gap: 1ch;
+  font-size: 3em;
+`
+
+const App = (): JSX.Element => (
+  <>
+    <Global styles={GlobalStyle} />
+    <Greeting>Good {timeOfDay()}, Amit</Greeting>
+  </>
+)
 
 export default App
