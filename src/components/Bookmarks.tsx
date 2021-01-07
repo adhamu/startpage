36089871fb32a022b6b1adfe66600922f5aacdc5
Amit Dhamu <amit.dhamu@dunelm.com>
2021-01-07 20:44:21 +0000

    Pure shit

--- src/components/Bookmarks.tsx
+++ src/components/Bookmarks.tsx
@@ -6,7 +6,7 @@ import links from '@global/links'
 const Bookmarks = styled.div`
   display: grid;
   align-items: center;
-  grid-template-columns: repeat(auto-fit, minmax(25%, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(20%, 1fr));
   margin-top: 3em;
   grid-gap: 0.2em;
   justify-content: space-between;
@@ -15,15 +15,13 @@ const Bookmarks = styled.div`
 const Bookmark = styled.div`
   text-align: left;
   color: ${props => props.theme.colors.heading};
-  display: block;
-  background: #18212e;
+  display: inline-block;
   padding: 0.1em 0.5em;
 
   a,
   a:visited {
     color: ${props => props.theme.colors.body};
     text-decoration: none;
-    display: block;
 
     &:hover {
       text-decoration: underline;
@@ -31,11 +29,20 @@ const Bookmark = styled.div`
   }
 `
 
+const Icon = styled.img`
+  display: inline-block;
+  vertical-align: middle;
+  line-height: 16px;
+  width: 16px;
+  height: 16px;
+  margin-right: 0.5em;
+`
+
 export default (): JSX.Element => (
   <Bookmarks>
-    {links.map((link, key: number) => (
+    {links.map((link, key) => (
       <Bookmark key={key}>
-        <img src={`${link.url.split(/[\\/]/).pop()}/favicon.ico`} />
+        <Icon src={`${new URL(link.url).origin}/favicon.ico`} />
         <a href={link.url} key={key}>
           {link.label}
         </a>
