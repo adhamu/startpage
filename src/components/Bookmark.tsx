0967aa086be5835cf7c2bbb5c66f40d3de1ec230
Amit Dhamu <dev@amitd.co>
2021-01-16 19:05:31 +0000

    Fix some shit

--- src/components/Bookmark.tsx
+++ src/components/Bookmark.tsx
@@ -21,8 +21,8 @@ const Icon = styled.img`
   vertical-align: middle;
   line-height: 16px;
   display: inline-block;
-  width: 20px;
-  height: 20px;
+  width: 24px;
+  height: 24px;
   margin-right: 0.5em;
 `
 
@@ -30,8 +30,8 @@ const PlaceholderContainer = styled.div`
   display: inline-block;
   margin-right: 0.5em;
   line-height: 16px;
-  width: 20px;
-  height: 20px;
+  width: 24px;
+  height: 24px;
   color: ${props => props.theme.colors.body};
 `
 
@@ -40,14 +40,17 @@ type Props = {
 }
 
 export default ({ bookmark }: Props): JSX.Element => {
+  const [error, setError] = React.useState(false)
   const {
     settings: { showFavicons },
   } = React.useContext(SettingsContext)
 
   return (
     <Bookmark href={bookmark.url} key={bookmark.id}>
-      {showFavicons && bookmark?.icon && <Icon src={bookmark?.icon} />}
-      {showFavicons && !bookmark?.icon && (
+      {showFavicons && bookmark?.icon && !error && (
+        <Icon src={bookmark?.icon} onError={() => setError(true)} />
+      )}
+      {showFavicons && (!bookmark?.icon || error === true) && (
         <PlaceholderContainer>
           <FontAwesomeIcon icon={faLink} fixedWidth />
         </PlaceholderContainer>
